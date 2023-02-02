f22ca959c783d02601a42e6eec790fe5c407c1de
Claeb101 <calebarulandu@gmail.com>
2023-02-02 15:25:20 -0500

    md example

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ VANTA.CLOUDS({
   minHeight: 200.00,
   minWidth: 200.00,
 })
+
+
+
+// fetch('/md').then(res => res.text()).then(d => console.log(d))
+
+// document.getElementById('content').innerHTML = DOMPurify.sanitize(marked.parse(`# Marked in the browser\n\nRendered by **marked**.`));
